a0564b4e3fa6bc7c4612280d24114b7bfb1117e5
csm-kr <csm8167@naver.com>
2016-10-04 21:12:55 +0900

    source1
    check it

---
+++ source1.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+
+void reverse1(double data[], int number);
+void reverse2(double *start, double *end);
+
+
+
+int main()
+{
+	int b = 1;
+	int a = 0;
+	if(a?b:1)
+	{
+		printf("%d");
+		printf("xx");
+	}
+}
